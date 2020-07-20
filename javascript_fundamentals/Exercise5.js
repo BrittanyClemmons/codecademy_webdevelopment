a663a900b50a46a1895dae29afe0d1084cc49e24
Brittany Clemmons <brittanyclemmons@gmail.com>
2020-07-19 22:16:29 -0500

    Exercise 5

---
+++ javascript_fundamentals/Exercise5.js
@@ -0,0 +1,17 @@
+// Exercise 5
+// Write a function, reportingForDuty(), 
+// that has two string parameters, rank and lastName, 
+// and returns a string in the following format: 
+// ‘rank lastName reporting for duty!’
+// Write your function here:
+
+// Write your function here:
+
+const reportingForDuty = (rank, lastname) => {
+  return rank +' '+ lastname + ' reporting for duty!';
+}
+
+// Uncomment the line below when you're ready to try out your function
+console.log(reportingForDuty('Private', 'Fido')) 
+
+// Should return 'Private Fido reporting for duty!'
